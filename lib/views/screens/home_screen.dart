d9c1de1728beac3f77565eebb853ca22b32225df
Prosper AMEBE <prosperamebe14@gmail.com>
2024-01-31 23:51:39 -0500

    start of the home button

--- lib/views/screens/home_screen.dart
+++ lib/views/screens/home_screen.dart
@@ -10,40 +10,59 @@ class HomeScreen extends StatelessWidget {
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: SafeArea(
-        child: Stack(
-          children: [
-            Container(
-              height: height,
-              width: width,
-              decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                colors: [
-                  Color.fromARGB(255, 30, 72, 110),
-                  Color.fromARGB(255, 102, 182, 225),
-                ],
-                transform: GradientRotation(-pi / 2),
-              )),
+      body: Container(
+          height: height,
+          width: width,
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 30, 72, 110),
+              Color.fromARGB(255, 102, 182, 225),
+            ],
+            transform: GradientRotation(-pi / 2),
+          )),
+          child: SafeArea(
+            child: Stack(
+              children: [
+                for (int i = 0; i < 10000; i++)
+                  Positioned(
+                      left: Random().nextDouble() * width,
+                      bottom: Random().nextDouble() * (height * 0.5),
+                      child: Container(
+                        width: 0.2,
+                        height: 0.2,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          border: Border.all(color: Colors.white, width: 1.0),
+                          borderRadius: BorderRadius.circular(2.5),
+                        ),
+                      )),
+                Image.asset(
+                  "assets/images/logo.png",
+                ),
+                Positioned(
+                    top: height * 0.3,
+                    left: width * 0.25,
+                    right: width * 0.25,
+                    child: Container(
+                      height: width * 0.15,
+                      width: width * 0.5,
+                      decoration: BoxDecoration(
+                          color: Color.fromARGB(255, 82, 191, 221)),
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Color.fromARGB(255, 82, 191, 221),
+                            foregroundColor: Colors.white),
+                        child: Text(
+                          "Classic Mode",
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ),
+                    ))
+              ],
             ),
-            for (int i = 0; i < 10000; i++)
-              Positioned(
-                  left: Random().nextDouble() * width,
-                  bottom: Random().nextDouble() * (height * 0.5),
-                  child: Container(
-                    width: 0.2,
-                    height: 0.2,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      border: Border.all(color: Colors.white, width: 1.0),
-                      borderRadius: BorderRadius.circular(2.5),
-                    ),
-                  )),
-            Image.asset(
-              "assets/images/logo.png",
-            )
-          ],
-        ),
-      ),
+          )),
     );
   }
 }
